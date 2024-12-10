dace510cc9c071aaf5eec9331a790fdc77666df5
Shrp <alex@sherpa.one>
2024-12-10 22:53:17 +0100

    cors

--- api/app/main.py
+++ api/app/main.py
@@ -2,13 +2,25 @@ from fastapi.responses import JSONResponse
 from fastapi.exceptions import RequestValidationError
 from fastapi.responses import PlainTextResponse, JSONResponse
 from fastapi import FastAPI, Request
+from fastapi.middleware.cors import CORSMiddleware
 
 from pydantic import ValidationError
+
 
 from .routers import task_router
 
 app = FastAPI()
 app.include_router(task_router.router)
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=[
+        "*"
+    ],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 
 @app.exception_handler(RequestValidationError)
